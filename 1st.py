410d3747757be3dc8fd9ab2497581870034e708e
Mahi Shah <mahishah1307@gmail.com>
2026-04-02 10:40:29 -0400

    work with file handling in python

--- 1st.py
+++ 1st.py
@@ -1,2 +1,8 @@
-print("Hi")
-print("This is Mahi")
+file = open("file1.txt","r")
+print(file.read())
+file.close()
+
+file = open("file2.txt","a")
+file.write("Mahi Shah")
+file.close()
+
---
+++ second.py
@@ -0,0 +1,2 @@
+with open("file3.txt","r") as Mahi:
+    print (Mahi.readlines())
